250094171a7f5e3389c64e58227435fdcc8cc50d
Muhammad Cavin Bagerry <cavin.bagerry@shopeepay.com>
2026-06-22 17:54:03 +0700

    feat(auth): integrate Better Auth with Cloudflare D1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,8 @@
+import { initOpenNextCloudflareForDev } from "@opennextjs/cloudflare";
 import type { NextConfig } from "next";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
+initOpenNextCloudflareForDev();
+
+const nextConfig: NextConfig = {};
 
 export default nextConfig;
